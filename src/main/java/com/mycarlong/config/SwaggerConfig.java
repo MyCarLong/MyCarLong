423c007e9d8062705cb3c40c8c6f8c3801301bb6
YRN <118609415+Kascald@users.noreply.github.com>
2024-04-22 17:37:23 +0900

    Swagger 적용 테스트 및 적용

--- src/main/java/com/mycarlong/config/SwaggerConfig.java
+++ src/main/java/com/mycarlong/config/SwaggerConfig.java
@@ -2,42 +2,28 @@ package com.mycarlong.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
-@EnableSwagger2
+
 @OpenAPIDefinition(
-		info = @Info(title = "MyCarLong API", version = "v0.1"),
-		security = @SecurityRequirement(name = "bearerAuth")
-)
+		info = @Info(title = "MyCarLong API 명세서",
+				description = "MyCarLong API 명세서",
+				version = "v0.1"))
 public class SwaggerConfig {
-		private static final String API_NAME = "MyCarLong API";
-		private static final String API_VERSION = "0.0.1";
-		private static final String API_DESCRIPTION = "MyCarLong API 명세서";
+	private static final String API_NAME = "MyCarLong API";
+	private static final String API_VERSION = "0.0.1";
+	private static final String API_DESCRIPTION = "MyCarLong API 명세서";
 
 	@Bean
-	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2)
-				.select()
-				.apis(RequestHandlerSelectors.any())
-				.paths(PathSelectors.any())
+	public GroupedOpenApi chatOpenApi() {
+		// "/v1/**" 경로에 매칭되는 API를 그룹화하여 문서화한다.
+		String[] paths = {"/**"};
+
+		return GroupedOpenApi.builder()
+				.group("MyCarLong API v0.1")  // 그룹 이름을 설정한다.
+				.pathsToMatch(paths)     // 그룹에 속하는 경로 패턴을 지정한다.
 				.build();
 	}
-
-		public ApiInfo apiInfo() {
-			return new ApiInfoBuilder()
-					.title(API_NAME)
-					.version(API_VERSION)
-					.description(API_DESCRIPTION)
-					.build();
-		}
-	}
-
+}
