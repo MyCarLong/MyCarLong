a51d8327dd6a44a914bc062f08bf1d3b0214c671
YRN <118609415+Kascald@users.noreply.github.com>
2024-05-01 14:56:26 +0900

    custom Exception adding. . . create commet , Swagger

---
+++ src/main/java/com/mycarlong/config/CustomException.java
@@ -0,0 +1,65 @@
+package com.mycarlong.config;
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/*
+* 이 클래스는 사용자 정의 예외처리를 위한 클래스 입니다.
+* 작성자 : 김근휘
+* 작성일시 : 2024-05-01
+* */
+@ControllerAdvice
+public class CustomException {
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "500", description = "WebDriver initialization failed")
+	})
+	@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR, reason = "WebDriver initialization failed")
+	public static class WebDriverInitializationException extends RuntimeException {
+		public WebDriverInitializationException(String message, Throwable cause) {
+			super(message, cause);
+		}
+	}
+
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "404", description = "Element not found")
+	})
+	@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Element not found")
+	public static class ElementNotFoundException extends RuntimeException {
+		public ElementNotFoundException(String message) {
+			super(message);
+		}
+	}
+
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "408", description = "Page load timeout")
+	})
+	@ResponseStatus(code = HttpStatus.REQUEST_TIMEOUT, reason = "Page load timeout")
+	public static class PageLoadTimeoutException extends RuntimeException {
+		public PageLoadTimeoutException(String message) {
+			super(message);
+		}
+	}
+
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "404", description = "Target not found")
+	})
+	@ResponseStatus(code = HttpStatus.NOT_FOUND, reason = "Target not found")
+	public static class TargetNotFoundException extends RuntimeException {
+		public TargetNotFoundException(String message) {
+			super(message);
+		}
+	}
+
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "500", description = "Error during Merging DATA")
+	})
+	@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR, reason = "Error during Merging DATA")
+	public static class MergingDataException extends RuntimeException {
+		public MergingDataException(String message) {
+			super(message);
+		}
+	}
+}
---
+++ src/main/java/com/mycarlong/service/WebDriverService.java
@@ -0,0 +1,64 @@
+package com.mycarlong.service;
+
+
+import com.mycarlong.config.CustomException;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.springframework.stereotype.Service;
+/**
+* 작성자 : 김근휘 <br>
+* 작성일시 : 2024 - 04 -30 <br>
+ * WebDriver의 LifeCycle 관리를 위한 클래스입니다.
+* */
+
+@Service
+@Tag(name="WebDriver Creation Service" , description = "If Not Initiated Any WebDriver Object Create New WebDriver , Manage Lifecycle")
+public class WebDriverService extends CustomException {
+	private WebDriver driver;
+
+	/**WebDriver가 없다면 새로운 크롬드라이버를 생성합니다. 설정은 내부 메소드의 설정을 따릅니다. <br>( 백그라운드에서 실행 등등 사전설정이 되어있음.)
+	 * */
+	@Operation(summary = "Get WebDriver instance", description = "Get the WebDriver instance. If it's null, initialize a new one.")
+	public WebDriver getDriver() {
+		if (driver == null) {
+			try {
+				driver = new ChromeDriver(setting());
+			} catch (Exception e) {
+				throw new WebDriverInitializationException("Failed to initialize WebDriver", e);
+			}
+		}
+		return driver;
+	}
+	/**열려있는 모든 driver를 닫습니다. driver를 모두 사용한 후에 반드시 필요한 작업입니다.
+	 * */
+	@Operation(summary = "Quit WebDriver instance", description = "Quit the WebDriver instance.")
+	public void quitDriver() {
+		if (driver != null) {
+			driver.quit();
+			driver = null;
+		}
+	}
+
+
+	@Operation(summary = "Set WebDriver options", description = "Set the options for WebDriver.")
+	private ChromeOptions setting() {
+		// 웹 드라이버 옵션 설정
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		options.addArguments("headless");
+		options.addArguments("disable-gpu");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--window-size=1920,1080");
+		options.addArguments("--disable-dev-shm-usage");
+
+		// 웹 드라이버 초기화
+		WebDriverManager.chromedriver().setup();
+		return options;
+	}
+}
+
+
