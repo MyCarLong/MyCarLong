cb8a2faec5cefa3b067a9dd8e9831b4802d2105e
KGH <kghui20@gmail.com>
2024-05-13 22:49:24 +0900

    aws s3 setting start

--- src/main/java/com/mycarlong/config/amazonS3Config.java
+++ src/main/java/com/mycarlong/config/amazonS3Config.java
@@ -1,5 +1,8 @@
 package com.mycarlong.config;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
@@ -9,20 +12,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class amazonS3Config (
+public class amazonS3Config {
 
-		@Value("${aws.s3.accessKey}")
-		private String accessKey,
-		@Value("${aws.s3.secretKey}")
-		private String secretKey;
-		) {
+	@Value("${cloud.aws.s3.region}")
+	private String region;
+
+	@Value("${cloud.aws.credentials.accessKey}")
+	private String accessKey;
+
+	@Value("${cloud.aws.credentials.secretKey}")
+	private String secretKey;
+
+	// 외부 의존성을 Bean으로 등록해서 DI를 통해 주입할 수 있도록 함
 	@Bean
-	fun amazonS3Client():AmazonS3 {
-		return AmazonS3ClientBuilder.standard()
-				.withCredentials(
-						AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey))
-				                )
-				.withRegion(Regions.AP_NORTHEAST_2)
-				.build()
+	public AmazonS3 amazonS3Client() {
+		AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+
+		return AmazonS3ClientBuilder
+				.standard()
+				.withCredentials(new AWSStaticCredentialsProvider(credentials))
+				.withRegion(region)
+				.build();
 	}
+
 }
