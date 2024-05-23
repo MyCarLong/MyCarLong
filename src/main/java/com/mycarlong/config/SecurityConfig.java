52f3f6760a2d4a74284bccbd4233ee2f16e8f333
KGH <kghui20@gmail.com>
2024-05-23 16:56:00 +0900

    cors update

--- src/main/java/com/mycarlong/config/SecurityConfig.java
+++ src/main/java/com/mycarlong/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.mycarlong.config;
 
-
+import java.util.Arrays;
 import java.util.Collections;
 
 import com.mycarlong.service.UserService;
@@ -14,6 +14,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.mycarlong.filter.JWTFilter;
 
@@ -21,71 +22,61 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig{
+public class SecurityConfig {
 
     private final UserService userService;
     private final CustomSuccessHandler customSuccessHandler;
     private final JWTUtil jwtUtil;
+    private final JWTFilter jwtFilter;
 
-    private final JWTFilter jwtFIlter;
-
-
-    public SecurityConfig(UserService userService, CustomSuccessHandler customSuccessHandler, JWTUtil jwtUtil, JWTFilter jwtFIlter) {
+    public SecurityConfig(UserService userService, CustomSuccessHandler customSuccessHandler, JWTUtil jwtUtil, JWTFilter jwtFilter) {
         this.userService = userService;
         this.customSuccessHandler = customSuccessHandler;
         this.jwtUtil = jwtUtil;
-        this.jwtFIlter = jwtFIlter;
+        this.jwtFilter = jwtFilter;
     }
 
-    // SecurityFilterChain을 생성하는 메서드입니다.
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, UserService userService) throws Exception {
-
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // CORS 설정
-        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
-            @Override
-            public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-                CorsConfiguration configuration = new CorsConfiguration();
-                configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
-                configuration.setAllowedMethods(Collections.singletonList("*"));
-                configuration.setAllowCredentials(true);
-                configuration.setAllowedHeaders(Collections.singletonList("*"));
-                configuration.setMaxAge(3600L);
-                configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
-                configuration.setExposedHeaders(Collections.singletonList("Authorization"));
-                return configuration;
-            }
-        }));
+        http.cors(corsCustomizer -> corsCustomizer.configurationSource(corsConfigurationSource()));
 
         // CSRF 비활성화
-        http.csrf((csrf) -> csrf.disable());
+        http.csrf(csrf -> csrf.disable());
 
         // HTTP Basic 인증 비활성화
-        http.httpBasic((httpBasic) -> httpBasic.disable());
+        http.httpBasic(httpBasic -> httpBasic.disable());
 
         // JWT 필터 추가
         http.addFilterAfter(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
         http.addFilterAfter(new JWTFilter(jwtUtil), OAuth2LoginAuthenticationFilter.class);
-    /*    // OAuth2 설정
-        http.oauth2Login((oauth2) -> oauth2
-                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
-                                .userService(customOAuth2UserService))
-                        .successHandler(customSuccessHandler)
-//                .defaultSuccessUrl("/testCookie")
-        );*/
 
         // 경로별 인가 작업
-        http.authorizeHttpRequests((authorizeRequests) -> authorizeRequests
-                .requestMatchers("/",",/aboutus","/api/**","oauth/**").permitAll()
-                .anyRequest().hasRole("USER") // 나머지 요청은 인증된 사용자만 허용
-        );
+        http.authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                                           .requestMatchers("/", "/aboutus", "/api/**", "/oauth/**").permitAll()
+                                           .anyRequest().hasRole("USER") // 나머지 요청은 인증된 사용자만 허용
+                                  );
 
         // 세션 관리 설정: STATELESS로 설정하여 세션을 사용하지 않습니다.
-        http.sessionManagement((sessionManagement) -> sessionManagement
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-        );
+        http.sessionManagement(sessionManagement -> sessionManagement
+                                       .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                              );
 
         return http.build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://mymcl.live","https://mymcl.live")); // 여러 도메인 추가
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type", "X-Requested-With"));
+        configuration.setExposedHeaders(Arrays.asList("Set-Cookie", "Authorization"));
+        configuration.setAllowCredentials(true);
+        configuration.setMaxAge(3600L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
 }
